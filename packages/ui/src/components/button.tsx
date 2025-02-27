8e31d2e20282b7c017007c287303c49e18c495ec
JoaoArouca <jarouca21@gmail.com>
2025-02-27 13:57:23 -0300

    refactor(ui): change to shadcn design tokens

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,32 +1,33 @@
-import { VariantProps } from 'class-variance-authority'
-
-import { clsxm } from '@/utils/clsxm'
+import { cn } from '@/lib/utils'
 import { Slot } from '@radix-ui/react-slot'
-import { cva } from 'class-variance-authority'
+import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-500 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        primary: 'bg-primary500 text-white shadow hover:bg-primary400',
-        secondary: 'bg-gray600 text-white shadow-sm hover:bg-gray500',
-        destructive: 'bg-danger500 text-white shadow-sm hover:bg-danger600',
+        default:
+          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-gray300 bg-transparent shadow-sm hover:bg-gray100',
-        ghost: 'hover:bg-gray100 hover:text-gray900',
-        link: 'text-primary500 underline-offset-4 hover:underline',
+          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-[2.5rem] px-[1rem] py-[0.5rem] text-md font-medium',
-        sm: 'h-[2rem] rounded-sm px-[0.75rem] text-sm',
-        lg: 'h-[3rem] rounded-md px-[1.5rem] text-lg',
-        icon: 'h-[2.5rem] w-[2.5rem]',
+        default: 'h-9 px-4 py-2',
+        sm: 'h-8 rounded-md px-3 text-xs',
+        lg: 'h-10 rounded-md px-8',
+        icon: 'h-9 w-9',
       },
     },
     defaultVariants: {
-      variant: 'primary',
+      variant: 'default',
       size: 'default',
     },
   }
@@ -43,7 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        className={clsxm(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
